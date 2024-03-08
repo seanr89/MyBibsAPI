873bb04d7b004d0cc00c30209f04ba6f9e48bdc4
seanr89 <srafferty89@gmail.com>
2024-03-08 23:04:07 +0000

    Add ClubsController and ClubService classes

---
+++ BibsAPI/Controllers/ClubsController.cs
@@ -0,0 +1,73 @@
+
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace YourNamespace
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ClubsController : ControllerBase
+    {
+        private readonly ClubService _clubService;
+        public ClubsController(ClubService clubService)
+        {
+            _clubService = clubService;
+        }
+        // GET: api/Clubs
+        [HttpGet]
+        public ActionResult<IEnumerable<Club>> GetClubs()
+        {
+            return _clubService.GetClubsAsync().Result.ToList();
+        }
+
+        // GET: api/Clubs/5
+        [HttpGet("{id}")]
+        public ActionResult<Club> GetClub(int id)
+        {
+            var club = _clubService.GetClubAsync(id).Result;
+            if (club == null)
+            {
+                return NotFound();
+            }
+            return club;
+        }
+
+        // POST: api/Clubs
+        [HttpPost]
+        public ActionResult<Club> PostClub(Club club)
+        {
+            var created = _clubService.AddClubAsync(club).Result;
+            return CreatedAtAction(nameof(GetClub), new { id = created.Id }, club);
+        }
+
+        // PUT: api/Clubs/5
+        // [HttpPut("{id}")]
+        // public IActionResult PutClub(int id, Club club)
+        // {
+        //     if (id != club.Id)
+        //     {
+        //         return BadRequest();
+        //     }
+        //     var existingClub = clubs.Find(c => c.Id == id);
+        //     if (existingClub == null)
+        //     {
+        //         return NotFound();
+        //     }
+        //     existingClub.Name = club.Name; // Update other properties as needed
+        //     return NoContent();
+        // }
+
+        // DELETE: api/Clubs/5
+        // [HttpDelete("{id}")]
+        // public IActionResult DeleteClub(int id)
+        // {
+        //     var club = clubs.Find(c => c.Id == id);
+        //     if (club == null)
+        //     {
+        //         return NotFound();
+        //     }
+        //     clubs.Remove(club);
+        //     return NoContent();
+        // }
+    }
+}
---
+++ BibsAPI/Services/ClubService.cs
@@ -0,0 +1,30 @@
+
+
+using Microsoft.EntityFrameworkCore;
+
+public class ClubService
+{
+    private AppDbContext _dbContext;
+
+    public ClubService(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<IEnumerable<Club>> GetClubsAsync()
+    {
+        return await _dbContext.Clubs.ToListAsync();
+    }
+
+    public async Task<Club> GetClubAsync(int id)
+    {
+        return await _dbContext.Clubs.FindAsync(id);
+    }
+
+    public async Task<Club> AddClubAsync(Club club)
+    {
+        _dbContext.Clubs.Add(club);
+        await _dbContext.SaveChangesAsync();
+        return club;
+    }
+}
