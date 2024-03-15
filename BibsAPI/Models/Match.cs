a05a8f335f8e5a7e92eccf1f342fb7b95f05a141
Sean Rafferty <srafferty89@gmail.com>
2024-03-15 18:02:29 +0000

    Add ClubId property to Match class and create MyBibsAPI solution file

--- BibsAPI/Models/Match.cs
+++ BibsAPI/Models/Match.cs
@@ -5,5 +5,6 @@ public class Match
     public int Id { get; set; }
     public DateTime Date { get; set; }
     public bool Complete { get; set; }
+    public Guid ClubId { get; set; }
     public Club Club { get; set; }
 }
